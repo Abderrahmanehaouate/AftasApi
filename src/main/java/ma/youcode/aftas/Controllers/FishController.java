357f4875c90550b8778a452f08e9a0a858812171
abderrahman haouate <abderrahmanehaouate@gmail.com>
2023-12-11 21:26:04 +0100

    crud of fish is created

--- src/main/java/ma/youcode/aftas/Controllers/FishController.java
+++ src/main/java/ma/youcode/aftas/Controllers/FishController.java
@@ -1,10 +1,15 @@
 package ma.youcode.aftas.Controllers;
 
+import ma.youcode.aftas.Models.Dtos.FishDto.FishRequestDto;
+import ma.youcode.aftas.Models.Entities.Fish;
 import ma.youcode.aftas.Services.FishService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RestController
@@ -14,5 +19,26 @@ public class FishController {
     @Autowired
     public FishController(FishService fishService) {
         this.fishService = fishService;
+    }
+    @GetMapping
+    public List<Fish> getAllFishes() {
+        return fishService.getAllFishes();
+    }
+    @GetMapping("/{id}")
+    public Fish getFishById(@PathVariable Long id) {
+        return fishService.getFishById(id);
+    }
+    @PostMapping("/create")
+    public Fish createFish(@RequestBody FishRequestDto fishDto) {
+        return fishService.createFish(fishDto);
+    }
+    @PutMapping("/update")
+    public Fish updateFish(@RequestBody FishRequestDto fishDto){
+        return fishService.updateFish(fishDto);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteFish(@PathVariable Long id){
+        fishService.deleteFishById(id);
+        return new ResponseEntity<>("Competition deleted successfully", HttpStatus.OK);
     }
 }
