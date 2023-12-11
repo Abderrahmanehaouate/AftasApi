981e4840578e5a25e2dd687ae2d54b95487dc284
abderrahman haouate <abderrahmanehaouate@gmail.com>
2023-12-11 20:52:43 +0100

    crud of competition is created

--- src/main/java/ma/youcode/aftas/Repositories/CompetitionRepository.java
+++ src/main/java/ma/youcode/aftas/Repositories/CompetitionRepository.java
@@ -2,9 +2,11 @@ package ma.youcode.aftas.Repositories;
 
 import ma.youcode.aftas.Models.Entities.Competition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CompetitionRepository extends JpaRepository<Competition, Long> {
-
 }
