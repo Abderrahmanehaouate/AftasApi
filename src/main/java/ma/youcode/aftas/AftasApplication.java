fad45410d567658ac7ba3d260be6b2c640cbd8a6
abderrahman haouate <abderrahmanehaouate@gmail.com>
2023-12-14 11:19:36 +0100

    last version stable

--- src/main/java/ma/youcode/aftas/AftasApplication.java
+++ src/main/java/ma/youcode/aftas/AftasApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AftasApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AftasApplication.class, args);
     }
-
 }
